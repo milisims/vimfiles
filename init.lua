81d86214467580758d328c6d8ff4312cc9e91233
Emilia Simmons <emilia.simmons@gmail.com>
2021-06-24 19:38:49 -0400

    Use vim.env to setup paths

--- init.lua
+++ init.lua
@@ -1,14 +1,6 @@
 
--- FIXME This is old and dumb. Change it
-vim.cmd([[
-let g:defaultdir=has('win32') ? $HOME.'\AppData\Local\nvim' : $HOME.'/.config/nvim'
-let $CFGDIR=empty($XDG_CONFIG_HOME) ? g:defaultdir : $XDG_CONFIG_HOME.'/nvim'
-
-let g:defaultdir=has('win32') ? $HOME.'\AppData\Local\nvim-data' : $HOME.'/.local/share/vim'
-let $DATADIR=empty($XDG_DATA_HOME) ? g:defaultdir : $XDG_DATA_HOME.'/nvim'
-unlet! g:defaultdir
-]])
-
+vim.env.CFGDIR = (vim.env.XDG_CONFIG_HOME or vim.env.HOME)..'/.config/nvim'
+vim.env.DATADIR = (vim.env.XDG_DATA_HOME or vim.env.HOME)..'/.local/share/nvim'
 vim.cmd('runtime settings.vim')
 
 require 'mia.plugin'
