7d9b9db006014de62709c3ceedbfa5ebbcd49f7b
Emilia Simmons <emilia.milisims@gmail.com>
2022-11-23 16:27:08 -0500

    feat: add T[10](func, [args]) indexing -> reps

--- plugin/functions.lua
+++ plugin/functions.lua
@@ -4,11 +4,31 @@ function mia.P(...)
   return v
 end
 
-function mia.T(...)
-  local t1 = os.clock()
-  select(1, ...)(select(2, ...))
-  mia.P(('Runtime: %fs'):format(os.clock() - t1))
-end
+
+mia.T = setmetatable({}, {
+  __call = function(self, ...)
+    self[1](...)
+  end,
+
+  __index = function(_, key)
+    if type(key) ~= 'number' then
+      error 'Indexing T must be done with a number'
+    end
+
+    return function(...)
+      local t1 = os.clock()
+      for _ = 1, key do
+        select(1, ...)(select(2, ...))
+      end
+      local end_t = os.clock() - t1
+      print(('Runtime: %fs (%d times)'):format(end_t, key))
+      if key > 1 then
+        print(('         %fs / call'):format(end_t/ key))
+      end
+    end
+  end
+})
+
 
 function mia.P1(...)
   local v = select(2, ...) and { ... } or ...
