efd323619f26eee0c2a5e9615daf9bd7576490a2
Emilia Simmons <emilia.milisims@gmail.com>
2023-04-07 16:34:53 -0400

    feat: add :Delview

--- plugin/commands.lua
+++ plugin/commands.lua
@@ -1,4 +1,6 @@
-vim.api.nvim_create_user_command('CloseFloatingWindows', function()
+local make_command = vim.api.nvim_create_user_command
+
+make_command('CloseFloatingWindows', function()
   local closed = {}
   for _, win in ipairs(vim.api.nvim_list_wins()) do
     local config = vim.api.nvim_win_get_config(win)
@@ -8,4 +10,17 @@ vim.api.nvim_create_user_command('CloseFloatingWindows', function()
     end
   end
   print('Windows closed: ', table.concat(closed, ' '))
+end, {})
+
+make_command('Delview', function()
+  local path = vim.fn.expand('%:p'):gsub('=', '==')
+  path = path:gsub('^' .. os.getenv('HOME'), '~')
+  path = path:gsub('/', '=+')
+  local file = vim.o.viewdir .. path .. '='
+  local success = vim.fn.delete(file) == 0
+  if success then
+    vim.api.nvim_echo({ { 'Deleted: ' .. file } }, true, {})
+  else
+    vim.api.nvim_echo({ { 'No view found: ' .. file, 'Error' } }, true, {})
+  end
 end, {})
