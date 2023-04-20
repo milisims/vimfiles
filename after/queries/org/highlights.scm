b4268b05493a927afcfb84d83083fce256b312d8
Emilia Simmons <emilia.milisims@gmail.com>
2023-04-20 15:08:34 -0400

    update: highlights

--- after/queries/org/highlights.scm
+++ after/queries/org/highlights.scm
@@ -1,3 +1,7 @@
 ; extends
 
+; (stars sub: _ @special)
 (stars sub: _ @conceal (#set! "conceal" "-"))
+
+(item . (_) @todo (#eq? @todo "GOAL"))
+(item . (_) @bold (#eq? @bold "CANCELLED")) @text.strike @comment
--- after/queries/python/highlights.scm
+++ after/queries/python/highlights.scm
@@ -1,9 +1,4 @@
 ; extends
 
-; quotes get darkened
-; ((string) @conceal (#match? @conceal "^('[^']|\"[^\"])"))
-; ((string) @string (#match? @string "^('[^']|\"[^\"])") (#offset! @string 0 1 0 -1))
-
-(string (string_content) @string) @dark
-(pair key: (string (string_content) @field))
+(pair key: (string (string_content) @field) @dark)
 
