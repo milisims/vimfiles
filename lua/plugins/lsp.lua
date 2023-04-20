8b55bef41d52f753774d3598c02a9261ab60bbf4
Emilia Simmons <emilia.milisims@gmail.com>
2023-04-20 15:00:15 -0400

    fix: lsp config. lazy=false, formatexpr aucmd, add tsserver

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,71 +1,70 @@
 return {
-  {
-    'williamboman/mason.nvim',
-    event = { 'InsertEnter', 'CursorHold' },
-    build = ':MasonUpdate',
-    dependencies = {
-      'onsails/lspkind-nvim',
-      'williamboman/mason-lspconfig.nvim',
-      'neovim/nvim-lspconfig',
-      'wbthomason/lsp-status.nvim',
-      'L3MON4D3/LuaSnip',
-    },
+  'williamboman/mason.nvim',
+  event = { 'InsertEnter', 'CursorHold' },
+  build = ':MasonUpdate',
+  lazy = false,
+  dependencies = {
+    'onsails/lspkind-nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+    'wbthomason/lsp-status.nvim',
+    'L3MON4D3/LuaSnip',
+  },
+  config = function()
+    -- needs to be before lspconfig
+    require('mason').setup()
+    require('mason-lspconfig').setup { ensure_installed = { 'pylsp', 'clangd' } }
+    local lspconfig = require('lspconfig')
 
-    config = function()
-      -- needs to be before lspconfig
-      require('mason').setup()
-      require('mason-lspconfig').setup { ensure_installed = { 'pylsp', 'clangd' } }
+    local lua_globals = {
+      'vim',
+      'mia',
+      'newproxy',                                                -- builtin
+      'P',                                                       -- mia, print vim.inspect
+      'use',                                                     -- packer
+      'eq', 're', 'pat', 'rep', 'rep1', 'choice',                -- nvim-org
+      'optional', 'describe', 'it', 'before_each', 'after_each', -- plenary
+      'lhs',                                                     -- contextualize
+    }
 
-      local lua_globals = {
-        'vim',
-        'mia',
-        'newproxy', -- builtin
-        'P', -- mia, print vim.inspect
-        'use', -- packer
-        'eq', 're', 'pat', 'rep', 'rep1', 'choice', -- nvim-org
-        'optional', 'describe', 'it', 'before_each', 'after_each', -- plenary
-        'lhs', -- contextualize
-      }
+    lspconfig.lua_ls.setup {
+      settings = {
+        Lua = {
+          runtime = { version = 'LuaJIT' }, -- lua version for neovim
+          diagnostics = {
+            globals = lua_globals,
+          },
+          -- Make the server aware of Neovim runtime files:
+          workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+          telemetry = { enable = false },
+        },
+      },
+    }
 
-      require('lspconfig').lua_ls.setup {
-        settings = {
-          Lua = {
-            runtime = { version = 'LuaJIT' }, -- lua version for neovim
-            diagnostics = {
-              globals = lua_globals,
-            },
-            -- Make the server aware of Neovim runtime files:
-            workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-            telemetry = { enable = false },
+    lspconfig.pylsp.setup {
+      settings = {
+        pylsp = {
+          plugins = {
+            pycodestyle = { maxLineLength = 100 },
           },
         },
-      }
+      },
+    }
 
-      require('lspconfig').pylsp.setup {
-        settings = {
-          pylsp = {
-            plugins = {
-              pycodestyle = { maxLineLength = 100 },
-            },
-          },
-        },
-      }
+    lspconfig.tsserver.setup({})
 
-      vim.diagnostic.config({ virtual_text = false, signs = true, underline = true })
+    vim.diagnostic.config({ virtual_text = false, signs = true, underline = true })
 
-      -- vim.api.nvim_create_augroup('mia-lsp', { clear = true })
-      -- vim.api.nvim_create_autocmd('LspAttach', {
-      --   group = 'mia-lsp',
-      --   desc = 'Format on save',
-      --   callback = function() vim.lsp.buf.signature_help() end,
-      -- })
-
-
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          vim.lsp.get_client_by_id(args.data.client_id).server_capabilities.semanticTokensProvider = nil
-        end,
-      })
-    end
-  }
+    vim.api.nvim_create_augroup('mia-lsp', { clear = true })
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = 'mia-lsp',
+      callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        -- client.server_capabilities.semanticTokensProvider = nil
+        if client.server_capabilities.documentFormattingProvider then
+          vim.bo[ev.buf].formatexpr = "v:lua.vim.lsp.formatexpr()"
+        end
+      end,
+    })
+  end
 }
