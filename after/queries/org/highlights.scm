1570fd8e57da8ca39fbe52597e54c27befdbfa1e
Emilia Simmons <emilia.milisims@gmail.com>
2023-06-14 13:33:09 -0400

    udpate(query): org highlights

--- after/queries/org/highlights.scm
+++ after/queries/org/highlights.scm
@@ -1,5 +1,9 @@
 ; extends
 
+(headline (stars) @_stars (#match? @_stars "^(\\*{3})*\\*$") (item) @identifier)
+(headline (stars) @_stars (#match? @_stars "^(\\*{3})*\\*\\*$") (item) @function)
+(headline (stars) @_stars (#match? @_stars "^(\\*{3})*\\*\\*\\*$") (item) @structure)
+
 (stars sub: _ @conceal (#set! conceal "-"))
 (stars final: _ @error (#set! conceal "❤")) ; U+2764 U+2765
 
@@ -7,7 +11,10 @@
 (item . (_) @bold (#eq? @bold "CANCELLED")) @text.strike @comment
 
 ; U+2713 U+2717
-(listitem (checkbox (status) @string (#eq? @string "/") (#set! conceal "✓")) . contents: (paragraph) @text.strike)
+(listitem (checkbox (status) @text.todo.checked (#eq? @text.todo.checked "/") (#set! conceal "✓")) . contents: (paragraph) @text.strike)
 (listitem (checkbox (status) @error (#eq? @error "X") (#set! conceal "✗")) . contents: (paragraph) @comment)
 (listitem (checkbox (status) @todo (#eq? @todo "-")) . contents: (paragraph [(str)(sym)(num)]+ @typedef))
+(listitem contents: (paragraph [(sym)(str)(num)]+ @tag . (sym ":") @special . (sym ":" prev: "sym" !next) @special))
+
+(body (paragraph (sym ":").(nl).) . (list (listitem (bullet) @boldconst (#eq? @boldconst "-") (#set! conceal "⤷")) ))
 (listitem contents: (paragraph [(sym)(str)(num)]+ @tag . (sym ":") @special . (sym ":" prev: "sym" !next) @special))
