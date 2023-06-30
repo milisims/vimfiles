544e417ac9dc4d2b45dd42e8da0490f345b27bdf
Emilia Simmons <emilia.milisims@gmail.com>
2023-06-30 15:26:14 -0400

    fix(skip): make it work good

--- lua/plugins/skip.lua
+++ lua/plugins/skip.lua
@@ -8,9 +8,29 @@ return {
   config = function(cfg)
     local flash = require 'flash'
     flash.setup(cfg.opts)
-    local map = require 'mapfun' 'm'
+    local map, omap = require 'mapfun' 'mo'
     map('s', flash.jump)
     map('S', flash.treesitter)
     map('ys', function() flash.jump { search = { mode = 'fuzzy' } } end)
+
+    -- label in operator mode with backdrop. see :h flash.nvim-flash.nvim-examples
+    -- by default, the operator maps are set, but we override them here.
+    local Config = require("flash.config")
+    local Char = require("flash.plugins.char")
+    for _, motion in ipairs({ "f", "t", "F", "T" }) do
+      omap(motion, function()
+        flash.jump(Config.get({
+          mode = 'char',
+          search = { mode = Char.mode(motion), max_length = 1 },
+          highlight = { backdrop = true },
+        }, Char.motions[motion]))
+      end)
+    end
+
+    for _, motion in ipairs { 'f', 't', 'F', 'T' } do
+      vim.keymap.set('o', motion, function()
+        flash.char { motion = motion, highlight = { backdrop = true } }
+      end)
+    end
   end,
 }
