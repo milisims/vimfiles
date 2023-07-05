6f07f6f20b6b79f4426b21ad66d8f90cefe752e3
Emilia Simmons <emilia.milisims@gmail.com>
2023-07-05 12:36:20 -0400

    fix(flash): use api for ,; - slightly hacky

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,4 +1,3 @@
-local last_flash, state, jumping
 return {
   'folke/flash.nvim',
   event = 'VeryLazy',
@@ -7,13 +6,22 @@ return {
     modes = {
       char = {
         highlight = { backdrop = false },
+        -- keys = { 'f', 'F', 't', 'T' },
+        char_action = function(motion)
+          -- not working?
+          return {
+            [','] = 'left',
+            [';'] = 'right',
+            [motion:upper()] = 'left',
+            [motion:lower()] = 'right',
+          }
+        end,
         config = function(opts)
           opts.autohide = vim.fn.mode(true):find 'no' and vim.v.operator == 'y'
           if vim.v.count == 0 and vim.fn.mode(true):find 'o' then
             opts.jump_labels = true
             opts.highlight.backdrop = true
           end
-          last_flash = 'char'
         end,
       },
     },
@@ -23,48 +31,32 @@ return {
     local flash = require 'flash'
     flash.setup(cfg.opts)
     local map, oxmap = require 'mapfun' 'mO'
+    local char = require('flash.plugins.char')
     map('s', function()
-      last_flash = 'jump'
-      flash.jump()
+      local state = flash.jump()
+      state.opts.highlight.backdrop = false
+      state.opts.label.after = false
+      char.state = state  -- hacky to get ,; to use their autocmd
+      char.state._mia = true
     end)
     map('S', flash.treesitter)
     oxmap('ys', flash.treesitter_search)
 
-    -- get mapped functions for , and ;
-    local c_backward = vim.fn.maparg(',', 'n', false, true).callback
-    local c_forward = vim.fn.maparg(';', 'n', false, true).callback
-
-    -- local state, jumping
-    vim.api.nvim_create_autocmd({ 'BufLeave', 'CursorMoved', 'InsertEnter' }, {
-      group = vim.api.nvim_create_augroup('mia_flash', { clear = true }),
-      callback = function(event)
-        if state and (event.event == "InsertEnter" or not jumping) then
-          state:hide()
-          state = nil
-        end
-      end,
-    })
-
-    local continue = function(forward)
+    local continue_jump = function(forward)
       return function()
-        if last_flash == 'jump' then
-          -- state = Repeat.get_state('jump', { continue = true })
-          state = require 'flash.repeat'._states['jump'] -- skip the 'show()'
-          state.opts.highlight.backdrop = false
-          state.opts.label.after = false
-          state:show()
-          jumping = true
-          state:jump { count = vim.v.count1, forward = forward }
-          vim.schedule(function() jumping = false end)
-        else
-          require 'flash.plugins.char'.state.opts.search.forward = true
-          (forward and c_forward or c_backward)()
+        if char.state._mia then
+          char.state:hide()  -- if repeating ,;
+          char.state.opts.search.forward = forward  -- used in labeling
         end
+        char.state:show()
+        char.jumping = true
+        char[forward and 'right' or 'left']()
+        vim.schedule(function() char.jumping = false end)
       end
     end
 
-    vim.keymap.set('n', ',', continue(false))
-    vim.keymap.set('n', ';', continue(true))
+    vim.keymap.set('n', ',', continue_jump(false))
+    vim.keymap.set('n', ';', continue_jump(true))
 
   end,
 }
