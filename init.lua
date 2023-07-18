b98f8faaaee7dd43d0aa5c6a5cce3f0d56f2a26f
Emilia Simmons <emilia.milisims@gmail.com>
2023-07-18 16:03:29 -0400

    chore: add comments

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
-require 'mia.conda'
-require 'mia.settings'
-require 'mia.autocmds'
-require 'mia.tslib'
-require 'mia.lazy'
-require 'mia.colors'
+require 'mia.conda'     -- set up python hostprog & repl
+require 'mia.settings'  -- vim settings
+require 'mia.autocmds'  -- basic autocmds, they're scattered about too where relevant
+require 'mia.tslib'     -- probably doesn't need to be here
+require 'mia.lazy'      -- jumpstart lazy install
+require 'mia.colors'    -- make sure hlgroups exist
