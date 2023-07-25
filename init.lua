88e33083e93a526876d7d2dbc99ccc3d3b59aca9
Emilia Simmons <emilia.milisims@gmail.com>
2023-07-25 11:31:35 -0400

    chore: move mia.stuff to plugin/

--- init.lua
+++ init.lua
@@ -1,6 +1,1 @@
-require 'mia.conda'     -- set up python hostprog & repl
-require 'mia.settings'  -- vim settings
-require 'mia.autocmds'  -- basic autocmds, they're scattered about too where relevant
-require 'mia.tslib'     -- probably doesn't need to be here
-require 'mia.lazy'      -- jumpstart lazy install
-require 'mia.colors'    -- make sure hlgroups exist
+require 'mia.lazy'
