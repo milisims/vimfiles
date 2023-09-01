83bf3c15e00cc6d868390c2f46a1e601395302f6
Emilia Simmons <emilia.milisims@gmail.com>
2023-09-01 15:29:01 -0400

    feat(doc): commands MinidocEnable and MinidocDisable

--- lua/plugins/minidoc.lua
+++ lua/plugins/minidoc.lua
@@ -4,15 +4,27 @@ return {
 
   config = function()
     nvim.create_augroup('mia-minidoc', { clear = true })
-    nvim.create_autocmd('BufWritePost', {
-      pattern = '*.lua',
-      group = 'mia-minidoc',
-      desc = 'Write docs on save',
-      callback = function()
-        if vim.fn.filereadable 'scripts/docgen.lua' == 1 then
-          dofile 'scripts/docgen.lua'
-        end
-      end,
-    })
+    local autocmd_id
+    nvim.create_user_command('MinidocEnable', function()
+      if autocmd_id then
+        nvim.del_autocmd(autocmd_id)
+      end
+      autocmd_id = nvim.create_autocmd('BufWritePost', {
+        pattern = '*.lua',
+        group = 'mia-minidoc',
+        desc = 'Write docs on save',
+        callback = function()
+          if vim.fn.filereadable 'scripts/docgen.lua' == 1 then
+            dofile 'scripts/docgen.lua'
+          end
+        end,
+      })
+    end, {})
+    nvim.create_user_command('MinidocDisable', function()
+      if autocmd_id then
+        nvim.del_autocmd(autocmd_id)
+      end
+      autocmd_id = nil
+    end, {})
   end,
 }
