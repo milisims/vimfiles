f90cda8ccf5ae25159fcadd861a83d32041f0bdc
Emilia Simmons <emilia.milisims@gmail.com>
2023-10-03 16:09:48 -0400

    fix: maybe fix autochdir

--- plugin/autochdir.lua
+++ plugin/autochdir.lua
@@ -15,17 +15,16 @@ local function gitdir(path, bufnr)
 end
 
 local gid = nvim.create_augroup('mia-chdir', { clear = true })
-nvim.create_autocmd({ 'BufRead', 'BufNewFile' }, {
+-- nvim.create_autocmd({ 'BufRead', 'BufNewFile' }, {
+nvim.create_autocmd('BufEnter', {
   group = gid,
   desc = ':lcd into git dir',
-  callback = function(ev) vim.cmd.lcd(gitdir(ev.match, ev.buf)) end,
-})
-
-nvim.create_autocmd('DirChanged', {
-  group = gid,
-  pattern = 'window',
-  desc = 'manually set autochdir',
-  callback = function(ev) vim.b[ev.buf].autochdir = vim.fn.getcwd() end,
+  callback = function(ev)
+    local bo = vim.bo[ev.buf]
+    if bo.modifiable and bo.buftype == '' then
+      vim.cmd.lcd(gitdir(ev.match, ev.buf))
+    end
+  end,
 })
 
 nvim.create_user_command('FixAutochdir', function()
