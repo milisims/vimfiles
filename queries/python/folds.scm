1ad4f2b6757dd2db65a6f8442f9baba691e7796e
Emilia Simmons <emilia.milisims@gmail.com>
2024-10-21 13:20:52 -0400

    fix: simple & clean python folding

--- queries/python/folds.scm
+++ queries/python/folds.scm
@@ -1,19 +1,8 @@
-; top level function and class definitions
-(module
-  [
-    (decorated_definition definition: (_ (block)))
-    (function_definition (block))
-    (class_definition (block))
-  ] @fold
-)
-(class_definition
-  (block
-    [
-     (decorated_definition (function_definition (block)))
-     (function_definition (block))
-    ] @fold
-  )
-)
+(decorated_definition) @fold
+([
+  (function_definition)
+  (class_definition)
+] @fold (#not-has-parent? @fold decorated_definition))
 
 (class_definition (block . (expression_statement . (string) @fold (#match? @fold "^\"\"\""))))
 (function_definition (block . (expression_statement . (string) @fold (#match? @fold "^\"\"\""))))
