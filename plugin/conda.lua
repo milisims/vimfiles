f0c13f2636812da7966da7b87c951140e57bdd6b
Emilia Simmons <emilia.milisims@gmail.com>
2024-10-21 13:16:31 -0400

    fix: add conda path to env. helps Mason

--- plugin/conda.lua
+++ plugin/conda.lua
@@ -1,9 +1,16 @@
 if not vim.env.CONDA_PREFIX then
   vim.api.nvim_echo({ { "conda env 'nvim-base' not found", 'Error' } }, true, {})
 else
-  _G._conda = {}
-  _conda.env = vim.env.CONDA_PREFIX
-  _conda.base = _conda.env:gsub('(/miniconda3)/.*$', '%1') .. '/envs/nvim-base'
-  _conda.hostprg = _conda.base .. '/bin/python'
+  local prefix = vim.env.CONDA_PREFIX:gsub('(/miniconda3)/.*$', '%1')
+
+  _G._conda = {
+    env = vim.env.CONDA_PREFIX,
+    base = prefix .. '/envs/nvim-base',
+    hostprg = prefix .. '/envs/nvim-base/bin/python',
+  }
+  local basebin = _conda.base .. '/bin'
+  if vim.env.CONDA_DEFAULT_ENV ~= 'nvim-base' and not vim.env.PATH:find(basebin, 1, true) then
+    vim.env.PATH = basebin .. ':' .. vim.env.PATH
+  end
   vim.g.python3_host_prog = _conda.hostprg
 end
