e179de439d193e7da37d1a11cc3ae5fbda764575
Emilia Simmons <emilia.milisims@gmail.com>
2025-01-22 15:40:57 -0500

    add: augrp hls. not pretty..

--- after/queries/lua/injections.scm
+++ after/queries/lua/injections.scm
@@ -2,3 +2,26 @@
 ;(function_call (identifier) @_name (#eq? @_name "set_query") (arguments . (string) @language (#offset! @language 0 1 0 -1) (string) @content . ))
 ;(function_call name: (_) @_func (#match? @_func "pyeval$") (arguments (string) @python (#match? @python "^\\[\\[") (#offset! @python 0 2 0 -2)))
 ;(function_call name: (_) @_func (#match? @_func "pyeval$") (arguments (string) @python (#match? @python "^[\"']") (#offset! @python 0 1 0 -1)))
+
+((function_call
+  name: (_) @_mia_augrp
+  arguments: (arguments
+    (table_constructor
+      (field
+        name: (_)
+        value: [
+                (string content: _ @injection.content)
+                (table_constructor (field !name value: (string content: _ @injection.content)))
+                (table_constructor (field !name value: (table_constructor (field !name value: (string content: _ @injection.content)))))
+                ]))))
+  (#set! injection.language "vim")
+  (#eq? @_mia_augrp "mia.augroup"))
+
+; ((function_call
+;   name: (_) @_vimcmd_identifier
+;   arguments: (arguments
+;     (string
+;       content: _ @injection.content)))
+;   (#set! injection.language "vim")
+;   (#any-of? @_vimcmd_identifier
+;     "vim.cmd" "vim.api.nvim_command" "vim.api.nvim_command" "vim.api.nvim_exec2"))
