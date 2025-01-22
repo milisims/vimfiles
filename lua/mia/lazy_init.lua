b042b90023cafcde3579a16d8582a04b63223a42
Emilia Simmons <emilia.milisims@gmail.com>
2025-01-22 17:34:45 -0500

    add: fallback for dev

--- lua/mia/lazy_init.lua
+++ lua/mia/lazy_init.lua
@@ -1,13 +1,13 @@
 -- lazy.nvim plugins
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
@@ -18,6 +18,6 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   spec = { import = 'plugins' },
   change_detection = { notify = false },
-  dev = { path = vim.fn.stdpath('config') .. '/mia_plugins' },
+  dev = { path = vim.fn.stdpath('config') .. '/mia_plugins', fallback = true },
   profiling = { require = true },
 })
