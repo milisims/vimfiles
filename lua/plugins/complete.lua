85f62ad27dd86d20e0a3fc3fb8c26bb27be3ef04
Emilia Simmons <emilia.milisims@gmail.com>
2025-01-29 19:05:36 -0500

    fix: blink completion & snippets

--- lua/plugins/complete.lua
+++ lua/plugins/complete.lua
@@ -2,6 +2,7 @@
 return {
   'saghen/blink.cmp',
   dependencies = 'rafamadriz/friendly-snippets',
+  event = 'InsertEnter',
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
@@ -10,15 +11,22 @@ return {
       preset = 'default',
       ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
       ['<Plug>(miaCmpConfirm)'] = { 'select_and_accept' },
-      cmdline = {
-        preset = 'default',
-        ['<Tab>'] = {},
-        ['<S-Tab>'] = {},
-        ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
-        ['<Plug>(miaCmpConfirm)'] = { 'select_and_accept' },
-      }
     },
-
+    sources = {
+      default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer', 'codecompanion' },
+      cmdline = {},
+      providers = {
+        lazydev = {
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
+          score_offset = 100,
+        },
+        codecompanion = {
+          name = 'CodeCompanion',
+          module = 'codecompanion.providers.completion.blink',
+        },
+      },
+    },
     appearance = { use_nvim_cmp_as_default = true },
   },
 }
