5a1237ef85563152a7bf54fda850de9b0653ce86
Emilia Simmons <emilia.milisims@gmail.com>
2025-02-01 14:55:54 -0500

    fix: blink cmdline completion

--- lua/plugins/complete.lua
+++ lua/plugins/complete.lua
@@ -2,25 +2,44 @@
 return {
   'saghen/blink.cmp',
   dependencies = 'rafamadriz/friendly-snippets',
-  event = 'InsertEnter',
+  event = { 'InsertEnter', 'CmdlineEnter' },
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
+
     keymap = {
       preset = 'default',
       ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
       ['<Plug>(miaCmpConfirm)'] = { 'select_and_accept' },
+      ['<C-Space>'] = {},
+
+      cmdline = {
+        preset = 'none',
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-c>'] = { 'cancel', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+      },
     },
+
+    completion = {
+      menu = {
+        auto_show = function(ctx)
+          return ctx.mode ~= 'cmdline'
+        end,
+      },
+    },
+
     sources = {
       default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer', 'codecompanion' },
-      cmdline = {},
       providers = {
         lazydev = {
           name = 'LazyDev',
           module = 'lazydev.integrations.blink',
           score_offset = 100,
         },
+        -- not necessary if not lazy loading codecompanion
         codecompanion = {
           name = 'CodeCompanion',
           module = 'codecompanion.providers.completion.blink',
