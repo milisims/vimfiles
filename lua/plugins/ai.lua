1401c6862b506a2fb93a89d03d050561fd0be709
Emilia Simmons <emilia.milisims@gmail.com>
2025-02-01 14:57:13 -0500

    add: ai chat keymaps & api key

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,9 +1,22 @@
 local pt = { provider = 'telescope' }
 local ot = { opts = pt }
+vim.env.GEMINI_API_KEY = mia.secrets.gemini()
 ---@type LazySpec
 return {
   'olimorris/codecompanion.nvim',
   cmd = { 'CodeCompanion', 'CodeCompanionActions', 'CodeCompanionChat', 'CodeCompanionCmd' },
+  ctx = {
+    {
+      mode = 'ca',
+      ctx = 'builtin.cmd_start',
+      each = {
+        cc = 'CodeCompanion',
+        cca = 'CodeCompanionActions',
+        cch = 'CodeCompanionChat',
+        ccc = 'CodeCompanionCmd',
+      },
+    },
+  },
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
@@ -13,6 +26,14 @@ return {
     strategies = {
       chat = {
         slash_commands = { buffer = ot, file = ot, symbols = ot, workspace = ot },
+
+        keymaps = {
+          completion = { modes = { i = '<Plug>(disabled)' } },
+          send = { modes = { n = '<C-g>', i = '<C-g><C-g>' } },
+          close = { modes = { n = '<Plug>(disabled)', i = '<Plug>(disabled)' } },
+          next_chat = { modes = { n = ']c' } },
+          previous_chat = { modes = { n = '[c' } },
+        },
       },
     },
   },
