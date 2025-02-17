153239b161ff9a9e3485076491eddc20e69535cd
Emilia Simmons <emilia.milisims@gmail.com>
2025-02-17 16:41:48 -0500

    update: layout config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,23 +1,22 @@
-local pick = {}
-do -- define funcs for prettier opts
-  function pick.cmd(name, opts)
+local pick = {
+  cmd = function(name, opts)
     if type(opts) == 'table' then
       opts = vim.inspect(opts, { newline = '', indent = '' })
     end
     return ('lua Snacks.picker.%s(%s)'):format(name, opts or '')
-  end
-
-  function pick.key(name, opts)
+  end,
+  key = function(name, opts)
     return function()
       _G.Snacks.picker[name](opts)
     end
-  end
-end
+  end,
+}
 
 return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
+  ---@module 'snacks'
   ---@type snacks.Config
   opts = {
     -- your configuration comes here
@@ -28,70 +27,84 @@ return {
     explorer = { enabled = true },
     indent = { enabled = true, indent = { char = '╎' } },
     input = { enabled = true },
-    notifier = { enabled = true },
+    notifier = {
+      enabled = true,
+      style = 'history',
+      top_down = false,
+    },
     quickfile = { enabled = true },
     scope = { enabled = true },
-    statuscolumn = { enabled = true }, -- ??
-    words = { enabled = true }, -- ??
+    statuscolumn = { enabled = true },
+    words = { enabled = false },  -- ??
+
     picker = {
       enabled = true,
+      layout = 'pseudo_sidebar',
+
       win = {
         input = {
-          keys = {
-            ['<C-t>'] = { 'tabdrop', mode = { 'i', 'n' } },
-            -- reverse for default swap
-            ['<Tab>'] = { 'select_and_prev', mode = { 'i', 'n' } },
-            ['<S-Tab>'] = { 'select_and_next', mode = { 'i', 'n' } },
+          keys = { ['<C-t>'] = { 'tabdrop', mode = { 'i', 'n' } } },
+        },
+      },
+      layouts = {
+        pseudo_sidebar = {  -- sidebar, but in floating windows.
+          layout = {
+            box = 'horizontal',
+            backdrop = false,
+            row = 1,
+            width = 0,
+            height = function()
+              return vim.o.lines - 3
+            end,
+            {
+              box = 'vertical',
+              width = 40,
+              {
+                win = 'input',
+                height = 1,
+                border = 'rounded',
+                title = ' {title} {live} {flags}',
+                title_pos = 'center',
+              },
+              { win = 'list', border = 'rounded' },
+            },
+            {
+              win = 'preview',
+              title = '{preview}',
+              border = 'rounded',
+              wo = {
+                wrap = true,
+              },
+            },
           },
         },
       },
       sources = {
-        config_files = {
-          -- git cfg ls-files. see milisims/dotfiles
-          finder = function(opts, ctx)
-            opts.cwd = vim.fs.normalize(vim.env.HOME)
-            ctx.picker:set_cwd(opts.cwd)
-            local args = { 'cfg', '-c', 'core.quotepath=false', 'ls-files', '--exclude-standard' }
-            return require('snacks.picker.source.proc').proc({
-              opts,
-              {
-                cmd = 'git',
-                args = args,
-                ---@param item snacks.picker.finder.Item
-                transform = function(item)
-                  item.cwd = opts.cwd
-                  item.file = item.text
-                end,
-              },
-            }, ctx)
-          end,
-          show_empty = true,
+        nvim_plugins = {
+          finder = 'files',
           format = 'file',
-          untracked = false,
-          submodules = false,
+          cwd = vim.fn.stdpath('data') .. '/lazy',
+          transform = function(item)
+            local plugin = item.file:match('^([^/]+)/')
+            item.cwd = item.cwd .. '/' .. plugin
+            item.file = item.file:sub(#plugin + 2)
+            return item
+          end,
         },
-      },
-      -- default layout but input on bottom
-      layouts = {
-        default = {
-          reverse = true,
-          layout = {
-            box = 'horizontal',
-            width = 0.8,
-            min_width = 120,
-            height = 0.9,
-            {
-              box = 'vertical',
-              border = 'rounded',
-              title = '{title} {live} {flags}',
-              { win = 'list', border = 'none' },
-              { win = 'input', height = 1, border = 'top' },
-            },
-            { win = 'preview', title = '{preview}', border = 'rounded', width = 0.5 },
-          },
+        config_files = {
+          format = 'file',
+          finder = function()
+            return vim
+                .iter(vim.fn.systemlist('git cfg ls-files --exclude-standard'))
+                :map(function(item)
+                  return { file = item, cwd = vim.env.HOME }
+                end)
+                :totable()
+          end,
         },
       },
     },
+
   },
 
   ctx = {
@@ -119,23 +132,21 @@ return {
         pws = pick.cmd('lsp_workspace_symbols'),
         ev = pick.cmd('files', '{ cwd = vim.fn.stdpath("config") }'),
         evp = pick.cmd('files', "{ cwd = vim.fn.stdpath('config') .. '/mia_plugins' }"),
-        evs = pick.cmd('files', "{ cwd = vim.fn.stdpath('data') .. '/lazy' }"),
         evr = pick.cmd('files', '{ cwd = vim.env.VIMRUNTIME }'),
-        gst = pick.cmd('git_status'),
+        evs = pick.cmd('nvim_plugins'),
         ecf = pick.cmd('config_files'),
+        gst = pick.cmd('git_status'),
       },
     },
   },
 
-  -- stylua: ignore start
   keys = {
-    { "gd", pick.key('lsp_definitions'), desc = "Goto Definition" },
-    { "gD", pick.key('lsp_declarations'), desc = "Goto Declaration" },
-    { "gr", pick.key('lsp_references'), nowait = true, desc = "References" },
-    { "gI", pick.key('lsp_implementations'), desc = "Goto Implementation" },
-    { "gy", pick.key('lsp_type_definitions'), desc = "Goto T[y]pe Definition" },
-    { "<C-g><C-o>", pick.key('jumps'), desc = "Pick jumps" },
-    { "z-", pick.key('spelling'), desc = "Pick spelling" },
+    { 'gd', pick.key('lsp_definitions'), desc = 'Goto Definition' },
+    { 'gD', pick.key('lsp_declarations'), desc = 'Goto Declaration' },
+    { 'gr', pick.key('lsp_references'), nowait = true, desc = 'References' },
+    { 'gI', pick.key('lsp_implementations'), desc = 'Goto Implementation' },
+    { 'gy', pick.key('lsp_type_definitions'), desc = 'Goto T[y]pe Definition' },
+    { '<C-g><C-o>', pick.key('jumps'), desc = 'Pick jumps' },
+    { 'z-', pick.key('spelling'), desc = 'Pick spelling' },
   },
-  -- stylua: ignore end
 }
