bc723160bda0bc633323fed67db33f44952ca57d
Emilia Simmons <emilia.milisims@gmail.com>
2025-03-02 14:00:01 -0500

    feat(snacks): add :Pick

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,21 +1,80 @@
-local pick = {
-  cmd = function(name, opts)
-    if type(opts) == 'table' then
-      opts = vim.inspect(opts, { newline = '', indent = '' })
-    end
-    return ('lua Snacks.picker.%s(%s)'):format(name, opts or '')
-  end,
-  key = function(name, opts)
-    return function()
-      _G.Snacks.picker[name](opts)
-    end
-  end,
-}
-
 return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
+  config = function(spec)
+    require('snacks').setup(spec.opts)
+
+    mia.command('Pick', {
+      nargs = '+',
+      callback = function(cmd)
+        local picker = cmd.fargs[1]
+
+        -- only support string arguments?
+        if not picker then
+          Snacks.picker.pick()
+        elseif #cmd.fargs == 1 then
+          Snacks.picker[picker]()
+        end
+
+        local opts = {}
+        for i = 2, #cmd.fargs do
+          local k, v = cmd.fargs[i]:match('^(%w+)=.*$') -- escaping ws works
+          if not k then
+            error('Invalid argument: ' .. cmd.fargs[i])
+          end
+          if v == 'true' then
+            v = true
+          elseif v == 'false' then
+            v = false
+          end
+          opts[k] = v
+        end
+
+        Snacks.picker[picker](opts)
+      end,
+
+      -- arglead, cmdline, cursorpos
+      complete = function(arglead, cmdline, _)
+        if cmdline == 'Pick ' then
+          return vim.tbl_keys(Snacks.picker.config.get().sources --[[@as table]])
+        end
+
+        local opts = Snacks.picker.config.get({ source = cmdline:match('Pick (%S+)') })
+
+        local opt = arglead:match('^(%w+)=')
+        if not opt then
+          local ret = { 'cwd=' }
+          local skip = { enabled = true, source = true }
+
+          for k, v in pairs(opts) do
+            if not skip[k] and type(v) ~= 'table' then
+              table.insert(ret, k .. '=')
+            end
+          end
+          return ret
+        end
+
+        if opt == 'focus' then
+          return { 'input', 'list' }
+        elseif opt == 'finder' then
+          return vim
+            .iter(Snacks.picker.config.get().sources)
+            :map(function(_, v)
+              return type(v.finder) == 'string' and v.finder or nil
+            end)
+            :totable()
+        elseif opt == 'layout' then
+          return vim.tbl_keys(Snacks.picker.config.get().layouts)
+        elseif opt == 'cwd' then
+          return vim.fn.getcompletion(arglead:sub(#opt + 2), 'file', true)
+        elseif type(opts[opt]) == 'boolean' then
+          return { 'true', 'false' }
+        end
+      end,
+    })
+  end,
+
   ---@module 'snacks'
   ---@type snacks.Config
   opts = {
@@ -35,7 +94,7 @@ return {
     quickfile = { enabled = true },
     scope = { enabled = true },
     statuscolumn = { enabled = true },
-    words = { enabled = false },  -- ??
+    words = { enabled = false }, -- ??
 
     picker = {
       enabled = true,
@@ -47,7 +106,7 @@ return {
         },
       },
       layouts = {
-        pseudo_sidebar = {  -- sidebar, but in floating windows.
+        pseudo_sidebar = { -- sidebar, but in floating windows.
           layout = {
             box = 'horizontal',
             backdrop = false,
@@ -72,9 +131,7 @@ return {
               win = 'preview',
               title = '{preview}',
               border = 'rounded',
-              wo = {
-                wrap = true,
-              },
+              wo = { wrap = true },
             },
           },
         },
@@ -95,16 +152,15 @@ return {
           format = 'file',
           finder = function()
             return vim
-                .iter(vim.fn.systemlist('git cfg ls-files --exclude-standard'))
-                :map(function(item)
-                  return { file = item, cwd = vim.env.HOME }
-                end)
-                :totable()
+              .iter(vim.fn.systemlist('git cfg ls-files --exclude-standard'))
+              :map(function(item)
+                return { file = item, cwd = vim.env.HOME }
+              end)
+              :totable()
           end,
         },
       },
     },
-
   },
 
   ctx = {
@@ -112,41 +168,41 @@ return {
       mode = 'ca',
       ctx = 'builtin.cmd_start',
       each = {
-        p = pick.cmd('smart'),
-        pi = pick.cmd('') .. '<Left><Left>',
-        pp = pick.cmd('pickers'),
-        f = pick.cmd('files'),
-        fh = pick.cmd('files', '{ cwd = vim.fn.expand("%:h") }'),
-        u = pick.cmd('undo'),
-        l = pick.cmd('buffers'),
-        pr = pick.cmd('resume'),
-        mr = pick.cmd('recent'),
-        A = pick.cmd('grep'),
-        h = pick.cmd('help'),
-        n = pick.cmd('notifications'),
-        ex = pick.cmd('explorer'),
-        hi = pick.cmd('highlights'),
-        em = pick.cmd('icons'),
-        t = pick.cmd('lsp_symbols'),
-        ps = pick.cmd('lsp_symbols'),
-        pws = pick.cmd('lsp_workspace_symbols'),
-        ev = pick.cmd('files', '{ cwd = vim.fn.stdpath("config") }'),
-        evp = pick.cmd('files', "{ cwd = vim.fn.stdpath('config') .. '/mia_plugins' }"),
-        evr = pick.cmd('files', '{ cwd = vim.env.VIMRUNTIME }'),
-        evs = pick.cmd('nvim_plugins'),
-        ecf = pick.cmd('config_files'),
-        gst = pick.cmd('git_status'),
+        p = 'Pick smart',
+        pi = 'Pick',
+        pp = 'Pick pickers',
+        f = 'Pick files',
+        fh = 'Pick files cwd=%:h',
+        u = 'Pick undo',
+        l = 'Pick buffers',
+        pr = 'Pick resume',
+        mr = 'Pick recent',
+        A = 'Pick grep',
+        h = 'Pick help',
+        n = 'Pick notifications',
+        ex = 'Pick explorer',
+        hi = 'Pick highlights',
+        em = 'Pick icons',
+        t = 'Pick lsp_symbols',
+        ps = 'Pick lsp_symbols',
+        pws = 'Pick lsp_workspace_symbols',
+        ev = 'Pick files cwd=<C-r>=stdpath("config")<Cr>',
+        evp = 'Pick files cwd=<C-r>=stdpath("config")<Cr>/mia_plugins',
+        evr = 'Pick files cwd=$VIMRUNTIME',
+        evs = 'Pick nvim_plugins',
+        ecf = 'Pick config_files',
+        gst = 'Pick git_status',
       },
     },
   },
 
   keys = {
-    { 'gd', pick.key('lsp_definitions'), desc = 'Goto Definition' },
-    { 'gD', pick.key('lsp_declarations'), desc = 'Goto Declaration' },
-    { 'gr', pick.key('lsp_references'), nowait = true, desc = 'References' },
-    { 'gI', pick.key('lsp_implementations'), desc = 'Goto Implementation' },
-    { 'gy', pick.key('lsp_type_definitions'), desc = 'Goto T[y]pe Definition' },
-    { '<C-g><C-o>', pick.key('jumps'), desc = 'Pick jumps' },
-    { 'z-', pick.key('spelling'), desc = 'Pick spelling' },
+    { 'gd', '<Cmd>Pick lsp_definitions<Cr>', desc = 'Goto Definition' },
+    { 'gD', '<Cmd>Pick lsp_declarations<Cr>', desc = 'Goto Declaration' },
+    { 'gr', '<Cmd>Pick lsp_references<Cr>', nowait = true, desc = 'References' },
+    { 'gI', '<Cmd>Pick lsp_implementations<Cr>', desc = 'Goto Implementation' },
+    { 'gy', '<Cmd>Pick lsp_type_definitions<Cr>', desc = 'Goto T[y]pe Definition' },
+    { '<C-g><C-o>', '<Cmd>Pick jumps<Cr>', desc = 'Pick jumps' },
+    { 'z-', '<Cmd>Pick spelling<Cr>', desc = 'Pick spelling' },
   },
 }
